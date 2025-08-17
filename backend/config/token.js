d5fa1f56acc834322d777092afb87c7f1f36158f
Rohit Varshney <136239562+Rohit-ekd@users.noreply.github.com>
2025-08-17 19:20:22 +0530

    Update token.js

--- backend/config/token.js
+++ backend/config/token.js
@@ -1,22 +1,3 @@
-// import jwt from "jsonwebtoken";
-
-// const genToken = async (userId) => {
-//   try {
-//     const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-//       expiresIn: "10d",
-//     });
-//     return token;
-//   } catch (error) {
-//     console.error("Error generating token:", error);
-//     throw new Error("Token generation failed");
-//   }
-// };
-// export default genToken;
-
-// // This function generates a JWT token for the user using their ID and a secret key from environment variables.
-// // The token is set to expire in 10 days. If an error occurs during token generation, it logs the error and throws an exception.
-
-
 import jwt from "jsonwebtoken";
 
 const genToken = (userId) => {
@@ -39,3 +20,4 @@ const genToken = (userId) => {
 };
 
 export default genToken;
+
